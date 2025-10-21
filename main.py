04150a68c4272ee5812e766abfed3eecb61fd47d
Jhunior Cuadros <jnrcuadrosr@gmail.com>
2025-10-21 10:44:24 -0500

    update main

--- main.py
+++ main.py
@@ -15,34 +15,5 @@ if __name__ == "__main__":
         p_movimiento=0.7  # Probabilidad de movimiento de cada monstruo
     )
 
-    simulacionE2.ejecutar(delay=0.2)
-
-    simulacionE1 = SimulacionEnergetica(
-        N=6,  # mismo tamaño
-        Nrobots=2,  # igual que el intermedio
-        Nmonstruos=2,  # igual que el intermedio
-        ticks=100,  # un poco menos: entorno más simple
-        K_monstruo=999,  # monstruos prácticamente inmóviles
-        seed=42,  # misma semilla para comparabilidad
-        Pfree=0.95,  # + espacios libres → menos colisiones
-        Psoft=0.05,  # - obstáculos
-        p_movimiento=0.05  # ~0: elimina el azar en los adversarios
-    )
-
-    simulacionE1.ejecutar(delay=0.2)
-
-    simulacionE3 = SimulacionEnergetica(
-        N=6,  # mismo tamaño
-        Nrobots=2,  # igual que el intermedio
-        Nmonstruos=2,  # igual que el intermedio
-        ticks=200,  # más largo: más interacción dinámica
-        K_monstruo=2,  # monstruos actúan con mayor frecuencia
-        seed=42,  # misma semilla para comparabilidad
-        Pfree=0.7,  # un poco menos de libres que el intermedio
-        Psoft=0.3,  # un poco más de obstáculos (sin ser extremo)
-        p_movimiento=0.9  # alta movilidad adversaria
-    )
-
     # Ejecución de la simulación
-    simulacionE3.ejecutar(delay=0.2)
-    #simulacion.ejecutar_manual_3d()
+    simulacionE2.ejecutar_manual_3d()
